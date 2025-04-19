4a916fc1f6188fe1ceca8c118adb8804bd80af74
Ahukko8 <ahukko88@gmail.com>
2025-04-19 16:31:02 +0500

    corrected i hink api bismillah

--- app/api/otherPdfs/[id]/route.ts
+++ app/api/otherPdfs/[id]/route.ts
@@ -23,9 +23,9 @@ const s3Client = new S3Client({
 // GET a single PDF by ID
 export async function GET(
   request: NextRequest,
-  context: { params: { id: string } }
+{ params }: { params: Promise<{ id: string }> }
 ) {
-  const id = context.params.id;
+  const id = await params;
 
   try {
     await connectDB();
@@ -45,9 +45,9 @@ export async function GET(
 // PUT: Update a PDF
 export async function PUT(
   request: NextRequest,
-  context: { params: { id: string } }
+  { params }: { params: Promise<{ id: string }> }
 ) {
-  const id = context.params.id;
+  const { id } = await params;
 
   try {
     const { userId } = await auth();
@@ -145,9 +145,9 @@ export async function PUT(
 // DELETE a PDF by ID
 export async function DELETE(
   request: NextRequest,
-  context: { params: { id: string } }
+  { params }: { params: Promise<{ id: string }> }
 ) {
-  const id = context.params.id;
+  const { id } = await params;
 
   try {
     const { userId } = await auth();
