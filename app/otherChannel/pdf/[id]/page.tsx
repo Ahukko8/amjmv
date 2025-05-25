d6ff01c71202f90b2f4395614b05fd1ced8fbf20
Ahukko8 <ahukko88@gmail.com>
2025-05-25 17:43:35 +0500

    update

--- app/otherChannel/pdf/[id]/page.tsx
+++ app/otherChannel/pdf/[id]/page.tsx
@@ -93,7 +93,7 @@ export default function OtherPDFReader({ params }: PDFPageProps) {
                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 10v6m0 0l-3-3m3 3l3-3m2 8H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z" />
               </svg>
             </a>
-              <Link href="/pdfs" className="inline-flex items-center px-6 py-3 bg-gray-600 text-white font-medium text-lg rounded-lg hover:bg-gray-700 transition-colors duration-200 shadow-lg hover:shadow-xl"
+              <Link href="/otherChannel/pdf" className="inline-flex items-center px-6 py-3 bg-gray-600 text-white font-medium text-lg rounded-lg hover:bg-gray-700 transition-colors duration-200 shadow-lg hover:shadow-xl"
 >
                 ފަހަތަށް
                 <ArrowLeftCircleIcon className="mr-2" />
